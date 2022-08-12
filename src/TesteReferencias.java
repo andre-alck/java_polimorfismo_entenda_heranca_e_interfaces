2e3d0220d92599c5fc018cc3c274e6b17fa226a9
andre-alck <andrealck1@gmail.com>
2022-08-11 23:45:44 -0300

    add cap3 - pt1

---
+++ src/TesteReferencias.java
@@ -0,0 +1,7 @@
+public class TesteReferencias {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Andre");
+        System.out.println(funcionario.getNome());
+    }
+}
