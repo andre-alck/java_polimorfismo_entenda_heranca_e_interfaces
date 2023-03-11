925eea08c362e8dcc445b5a6f65f09705f4c57f4
andre-alck <andrealck1@gmail.com>
2023-03-11 20:43:43 -0300

    build: reinit project

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,11 +1,8 @@
-public class Funcionario {
+public class Cliente {
+
     private String nome;
     private String cpf;
-    protected double salario;
-
-    public double getBonificacao() {
-        return this.salario * 0.1;
-    }
+    private String profissao;
 
     public String getNome() {
         return nome;
@@ -23,12 +20,12 @@ public class Funcionario {
         this.cpf = cpf;
     }
 
-    public double getSalario() {
-        return salario;
+    public String getProfissao() {
+        return profissao;
     }
 
-    public void setSalario(double salario) {
-        this.salario = salario;
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
     }
 
 }
--- src/TesteReferencias.java
+++
@@ -1,7 +0,0 @@
-public class TesteReferencias {
-    public static void main(String[] args) {
-        Funcionario funcionario = new Funcionario();
-        funcionario.setNome("Andre");
-        System.out.println(funcionario.getNome());
-    }
-}
